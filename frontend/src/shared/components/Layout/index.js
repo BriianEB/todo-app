3ebea8771fc735b2ed12e959ddb86e53d957caa0
Brian Becerra <elibecerrahdz@gmail.com>
2023-06-25 18:23:07 -0600

    frontend

--- frontend/src/shared/components/Layout/index.js
+++ frontend/src/shared/components/Layout/index.js
@@ -1,5 +1,8 @@
 import { styled } from '@mui/material/styles';
 
+import Header from './Header';
+import Footer from './Footer';
+
 
 const Container = styled('div')({
   background: 'url(/background.svg)',
